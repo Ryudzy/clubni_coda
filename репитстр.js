6c1bc0ca6500feb445617a64dcdc2d7b5b8d552b
Ryudzy <anna_bba2404@mail.ru>
2023-03-18 14:53:53 +0300

    Domashka repitstr i finalchetnechet

---
+++ репитстр.js
@@ -0,0 +1,6 @@
+function repeatStr(str, n) {
+   let newstr = '';
+    while (n-- > 0) newstr += str;
+    return newstr; }
+    
+    repeatStr("Hello world!",2) 
---
+++ финалчетнечет.js
@@ -0,0 +1,8 @@
+let num = prompt('Введите число', 100)
+if (num%2 == true)  {
+alert(`нечетное`)}
+    if (num%2 == false) {
+alert('четное') }
+else  {
+    alert(`Ошибка! Введите целое число`) 
+}
